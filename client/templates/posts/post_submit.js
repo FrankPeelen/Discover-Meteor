1d05ba15608518afb21cae253da8dcf842db51ba
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-11 15:28:00 +0900

    Implement error reporting

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,10 +9,10 @@ Template.postSubmit.events({
 
 		Meteor.call('postInsert', post, function(error, result) {
 			// display the error to the user and abort
-			if (error) { return alert(error.reason);post._id = Posts.insert(post); }
+			if (error) { return throwError(error.reason); }
 
 			// show this result but route anyway
-			if (result.postExists) { alert('This link has already been posted'); }
+			if (result.postExists) { throwError('This link has already been posted'); }
 
 			Router.go('postPage', post);
 		});
